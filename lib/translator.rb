01dc9173c67cf9fafa3b04e21ac63abaae31927b
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-29 00:34:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,9 @@
 require "yaml"
 
 def load_library(library)
-  YAML.load_file(library)
+  emoticon_library = YAML.load_file(library)
+  meaning_array = emoticon_library.reduce([]) { |memo, (meaning, emo)| memo << meaning; memo }
+
 end
 
 def get_japanese_emoticon
